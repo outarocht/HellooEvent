8899276d625da3a6939bdd0b55779fbabd202fe0
zOutarocht <39389424+zOutarocht@users.noreply.github.com>
2020-08-22 04:28:50 +0200

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -22,43 +22,35 @@ var app = {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-		var pushNotification = window.plugins.pushNotification;
+		// deviceready Event Handler
+		//
+		// Bind any cordova events here. Common events are:
+		// 'pause', 'resume', etc.
+		onDeviceReady: function() {
+			var pushwoosh = cordova.require("pushwoosh-cordova-plugin.PushNotification");
 
-		//set push notification callback before we initialize the plugin
-		document.addEventListener('push-notification', function(event) {
-			//get the notification payload
-			var notification = event.notification;
+	// Should be called before pushwoosh.onDeviceReady
+	document.addEventListener('push-notification', function(event) {
+		var notification = event.notification;
+		// handle push open here
+	});
 
-			//display alert to the user for example
-			alert(notification.aps.alert);
+	// Initialize Pushwoosh. This will trigger all pending push notifications on start.
+	pushwoosh.onDeviceReady({
+		appid: "2DEB0-FBD3B",
+		projectid: "533095191225",
+		serviceName: "XXXX"
+	});
 
-			//clear the app badge
-			pushNotification.setApplicationIconBadgeNumber(0);
-		});
-
-		//initialize the plugin
-		pushNotification.onDeviceReady({appid:"2DEB0-FBD3B"});
-
-		//register for pushes
-		pushNotification.registerDevice(
-			function(status) {
-				var deviceToken = status['deviceToken'];
-				console.warn('registerDevice: ' + deviceToken);
-			},
-			function(status) {
-				console.warn('failed to register : ' + JSON.stringify(status));
-				alert(JSON.stringify(['failed to register ', status]));
-			}
-		);
-
-		//reset badges on app start
-		pushNotification.setApplicationIconBadgeNumber(0);
-		
+	pushwoosh.registerDevice(
+		function(status) {
+			var pushToken = status.pushToken;
+			// handle successful registration here
+	  },
+	  function(status) {
+		// handle registration error here
+	  }
+	);
         this.receivedEvent('deviceready');
 		
     },
