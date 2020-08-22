af37730b82f2b45834d3d690d1f1e16c68f7777c
zOutarocht <39389424+zOutarocht@users.noreply.github.com>
2020-08-22 03:43:56 +0200

    update

---
+++ plugins/pushwoosh-cordova-plugin/src/ios/PushwooshInboxUI.framework/Versions/A/Headers/PWIInboxUI.h
@@ -0,0 +1,34 @@
+//
+//  PWIInboxUI.h
+//  PushwooshInboxUI
+//
+//  Created by Pushwoosh on 01/11/2017.
+//  Copyright © 2017 Pushwoosh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@protocol PWInboxMessageProtocol;
+
+@interface PWIInboxViewController : UIViewController
+
+@property (nonatomic) void (^onMessageClickBlock)(id<PWInboxMessageProtocol> message);
+
+/**
+ Reloads everything from scratch.
+ */
+- (void)reloadData;
+- (instancetype)init NS_UNAVAILABLE;
+
+@end
+
+@class PWIInboxStyle;
+@interface PWIInboxUI : NSObject
+
+/**
+ @return PWIInboxViewController with a specified style
+ */
++ (PWIInboxViewController *)createInboxControllerWithStyle:(PWIInboxStyle *)style;
+
+@end
